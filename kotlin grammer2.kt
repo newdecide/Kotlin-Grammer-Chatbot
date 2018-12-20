3b84d4d9f3d21d17978b7922283bbca069bffbce
newdecide <liberty-1@nate.com>
2018-12-21 00:12:35 +0900

    Create kotlin grammer2.kt

---
+++ kotlin grammer2.kt
@@ -0,0 +1,21 @@
+fun main() {
+  // Double 64bit, Float 32bit, Long 64bit, Int 32bit, Short 16bit,Byte8bit
+   		var a: Int = 3
+        a = 5
+        var b: Int = 4
+        var c: Int = a + b
+        println(c)
+
+        // Character
+        var C: Char = 'z'
+        println(C)
+
+        // Boolean 논리값 true, false
+        var d: Boolean = a > b // 5>4 true
+        println(d)
+        var e: Boolean = b > a // 4>5 false
+        println(e)
+
+        // String 문자열
+        var string: String = "나는 문자열 입니다."
+}
